918de293abf77922851a8b69c6817fc8b9506df3
Samyak <srai890890@gmail.com>
2025-10-10 15:56:11 +0530

    Added Data Generator to CSV

---
+++ Data_Gen/generator.py
@@ -0,0 +1,51 @@
+import numpy as np
+import pandas as pd
+EPS = 1e-9 # Protection division by zero
+
+
+# Create Noisy Data for given set of Parameters
+
+def Create_Problem_function(alpha, beta, gamma):
+    """
+    Model Function of Gravitational Wave given in Problem Statement
+
+    :param alpha: Parameter Alpha (0, 2)
+    :param beta: Parameter Beta (1, 10)
+    :param gamma: Parameter Gamma (1, 20)
+    :return: Gravitational Wave Function for given Params
+    """
+    def Gravitational_Wave(t):
+        return alpha * np.exp(t) * (1 - np.tanh(2 * (t - beta))) * np.sin(gamma * t)
+
+    return Gravitational_Wave
+
+
+def Generate_Data(function, file_name: str, noise = 0.2, t_min = 0, t_max = 15, num = 1000):
+    """
+    Generates Time Series Noisy Data for given function
+
+    :param function: Input Model Function
+    :param file_name: Name of file to Save
+    :param noise: Noise in Datapoints
+    :param t_min: time lower bound
+    :param t_max: time upper bound
+    :param num: Number of Datapoints
+    :return: Time Series Data ( Time, Datapoints )
+    """
+    time = np.linspace(t_min, t_max, num)
+    f_points = function(time)
+    f_error = noise * f_points + EPS
+    f_noisy = f_points + np.random.normal(0, np.abs(f_error), num)
+
+    df = pd.DataFrame({
+        'time': time,
+        'datapoint': f_noisy
+    })
+
+    df.to_csv(f"Data_{file_name}.csv", index = False, float_format='%.6f')
+
+    return time, f_points
+
+
+Generate_Data(Create_Problem_function(0.3, 5, 10),"Grav_Wave",
+              noise = 0.2, t_min = 0, t_max = 15, num = 1001)
