c2e4cddc72721a67f8ad6f998e14fa2d8d345358
Samyak <srai890890@gmail.com>
2025-10-11 14:14:58 +0530

    Added plot saves and datapoints vs function plotting

--- Source/plotting.py
+++ Source/plotting.py
@@ -1,36 +1,39 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from pathlib import Path
 
-def GW_Plot():
+def histogram_gw(true_params, mh_chain, file: Path):
     fig, ax = plt.subplots(2, 3, figsize=(24, 12))
     labels = ['alpha', 'beta', 'gamma']
-    for j in range(1):
-        seed = int(1000*np.random.random())
-        print(seed)
-        MH_chain, Proposal_chain = MHSampling(seed=seed)
-        for i in range(3):
-            # Compute histogram first
-            counts, bins = np.histogram(MH_chain[:, i], bins=50)
 
-            # Plot the histogram
-            ax[0, i].hist(MH_chain[:, i], bins=50, density=True)
-            ax[0, i].set_title(f"{labels[i]}, seed = {seed}")
-            ax[0, i].axvline(true_params[i], color='r', linestyle='--', label="True Value")
+    for i in range(3):
+        # Compute histogram first
+        counts, bins = np.histogram(mh_chain[:, i], bins=50)
 
-            # Find the bin with maximum count (mode)
-            max_count_idx = np.argmax(counts)
-            # Get the center of that bin
-            pred_value = (bins[max_count_idx] + bins[max_count_idx + 1]) / 2
+        # Plot the histogram
+        ax[0, i].hist(mh_chain[:, i], bins=50, density=True)
+        ax[0, i].set_title(f"{labels[i]}, seed = {seed}")
+        ax[0, i].axvline(true_params[i], color='r', linestyle='--', label="True Value")
+        # Pred value
+        max_count_idx = np.argmax(counts)
+        pred_value = (bins[max_count_idx] + bins[max_count_idx + 1]) / 2
 
-            ax[0, i].axvline(pred_value, color='g', linestyle='--', label='Predicted Value')
-            print(f"Predicted Value of {labels[i]} at {true_params[i]} is {pred_value:.2f}")
-            ax[0, i].legend()
-            ax[0, i].grid(True)
-        rejection_plots = False
-        ax[1, 0].plot(MH_chain[:, 0])
-        if rejection_plots: ax[1, 0].scatter(np.arange(0,MH_chain.shape[0], 1), Proposal_chain[:, 0], s = 0.2)
-        ax[1, 1].plot(MH_chain[:, 1])
-        if rejection_plots: ax[1, 1].scatter(np.arange(0,MH_chain.shape[0], 1),Proposal_chain[:, 1], s = 0.2)
-        ax[1, 2].plot(MH_chain[:, 2])
-        if rejection_plots: ax[1, 2].scatter(np.arange(0,MH_chain.shape[0], 1), Proposal_chain[:, 2], s = 0.2)
+        ax[0, i].axvline(pred_value, color='g', linestyle='--', label='Predicted Value')
+        print(f"Predicted Value of {labels[i]} at {true_params[i]} is {pred_value:.2f}")
+        ax[0, i].legend()
+        ax[0, i].grid(True)
+    ax[1, 0].plot(mh_chain[:, 0])
+    ax[1, 1].plot(mh_chain[:, 1])
+    ax[1, 2].plot(mh_chain[:, 2])
     plt.tight_layout()
-    plt.show()
+
+    path = Path(file)
+    plt.savefig(path, bbox_inches='tight')
+
+def data_points(datapoints, function, file: Path):
+    plt.figure(figsize=(24, 12))
+    plt.scatter(datapoints[:, 0], datapoints[:, 1], label = "Datapoints")
+    model_data = function(datapoints[:, 0])
+    plt.plot(datapoints[:, 0], model_data, label = "Model function")
+    path = Path(file)
+    plt.savefig(path, bbox_inches='tight')
